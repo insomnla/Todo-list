af64d8e4fcda629ee208722e74e577da8f2d0fec
insomnla <Epikmen@bk.ru>
2021-03-02 20:36:17 +0700

    upd - updated way of showing popup windows

--- todolist/views/script/main.js
+++ todolist/views/script/main.js
@@ -14,10 +14,16 @@ let symbol = document.querySelectorAll(".symbol");
 let contentHiddenItem = document.querySelectorAll(".mw-content__hidden-item");
 
 
-
 tasksCard.forEach((elem)=>{
     elem.addEventListener('click', ()=>{
         modalTasks.classList.remove('dn');
+        let arr = elem.dataset['key'].split(' ');
+        let title = arr[0];
+        let desc = elem.dataset['desc'];
+        let deadline = setDeadline(elem);
+        document.querySelector(".mw-top__title").textContent = title;
+        document.querySelector(".mw-top__deadline").textContent = deadline;
+        document.querySelector(".mw-content__description").textContent = desc;
     });
 });
 
@@ -58,13 +64,37 @@ symbol.forEach((elem)=>{
             elem.classList.add('tilt');
             elem.classList.remove('reverseTilt');
         }
-        let array = new Array;
-        array = elem.textContent.split('.');
+        let key = elem.dataset['key'];
         contentHiddenItem.forEach((elem)=>{
-            let key = elem.dataset['key'];
-            if(+key == array[0]) {
-                elem.classList.toggle('dn');
+            let hiddenKey = elem.dataset['key'];
+            if(+key == hiddenKey) {
+                if (elem.classList.contains('dn')){
+                    elem.firstChild.nextElementSibling.style.animation = 'slowDown .6s linear forwards';
+                    elem.classList.remove('dn');
+                } else {
+                    elem.firstChild.nextElementSibling.style.animation = 'slowUp .6s linear forwards';
+                    setTimeout(() => elem.classList.add('dn'), 600);
+                }
             }
         });
     });
 });
+
+function setDeadline(object) {
+    let monthsArray = new Array ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+
+    let arr = object.dataset['date'].split(' ');
+    let day = arr[2];
+    let monthName = arr[1];
+    let year = arr[3];
+
+    let month = monthsArray.findIndex((i)=> i === monthName) + 1;
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    let date = `${day}.${month}.${year}`;
+
+    return date;
+}
