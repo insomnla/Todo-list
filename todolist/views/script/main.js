3d6a79dcf70b231ea893e62472b1078ab768e007
insomnla <Epikmen@bk.ru>
2021-03-09 12:33:41 +0700

    add - new animations

--- todolist/views/script/main.js
+++ todolist/views/script/main.js
@@ -4,19 +4,26 @@ let modalNewTasks = document.querySelector(".modal--newTasks")
 let modalError = document.querySelector(".modal--error");
 
 
+let formNewtasks = document.querySelector('.form--newTasks');
+let formCreateTask =document.querySelector('.form-createTask');
+
+
 let buttonExit = document.querySelectorAll(".mw-bottom__exit");
 let buttonNewTask = document.querySelector(".bp-item--new-task");
 let buttonAdd = document.querySelector(".mw-bottom__add");
+let buttonCreateTask = document.querySelector(".mw-top__button");
+let buttonCreateBack = document.querySelector(".mw-top__button-back");
 
 
-let formNewTasks = document.querySelector(".form--newTasks");
 let symbol = document.querySelectorAll(".symbol");
 let contentHiddenItem = document.querySelectorAll(".mw-content__hidden-item");
+let dateInNewTasks = document.querySelectorAll(".task-chek__deadline");
 
 
 tasksCard.forEach((elem)=>{
     elem.addEventListener('click', ()=>{
         modalTasks.classList.remove('dn');
+        modalTasks.firstChild.nextSibling.style.animation = 'popUp .4s ease-in-out forwards';
         let arr = elem.dataset['key'].split(' ');
         let title = arr[0];
         let desc = elem.dataset['desc'];
@@ -27,31 +34,50 @@ tasksCard.forEach((elem)=>{
     });
 });
 
+console.dir(modalTasks.firstChild.nextSibling);
+
 buttonExit.forEach((elem)=>{
     elem.addEventListener('click', ()=>{
         if(!modalTasks.classList.contains('dn')){
-            modalTasks.classList.add('dn');
-        };
-        if(!modalNewTasks.classList.contains('dn')){
-            modalNewTasks.classList.add('dn');
-        };
-        if(!modalError.classList.contains('dn')){
-            modalError.classList.add('dn');
+            modalTasks.firstChild.nextSibling.style.animation = 'popDown .4s ease-in-out forwards';
+            setTimeout(()=>{
+                modalTasks.classList.add('dn');
+            }, 400)
+        } else if(!modalNewTasks.classList.contains('dn')){
+            modalNewTasks.firstChild.nextSibling.style.animation = 'popDown .4s ease-in-out forwards';
+            setTimeout(()=>{
+                modalNewTasks.classList.add('dn');
+            }, 400)
+        } else if(!modalError.classList.contains('dn')){
+            modalError.firstChild.nextSibling.style.animation = 'popDown .4s ease-in-out forwards';
+            setTimeout(()=>{
+                modalError.classList.add('dn');
+            }, 400)
         };
     });
 });
 
-formNewTasks.addEventListener('submit', (event)=>{
-   // event.preventDefault(); - :okek:
-});
-
 buttonNewTask.addEventListener('click', ()=>{
     modalNewTasks.classList.remove('dn');
+    modalNewTasks.firstChild.nextSibling.style.animation = 'popUp .4s ease-in-out forwards';
+    dateInNewTasks.forEach((elem)=>{
+        elem.textContent = setDeadline(elem);
+    });
 });
 
 buttonAdd.addEventListener('click', ()=>{
     modalError.classList.remove('dn');
+    modalError.firstChild.nextSibling.style.animation = 'popUp .4s ease-in-out forwards';
 }); 
+
+buttonCreateTask.addEventListener('click', ()=>{
+    document.querySelector(".slider-line").style.right = '710px';
+    formCreateTask.classList.remove('dn');
+});
+
+buttonCreateBack.addEventListener('click', ()=>{
+    document.querySelector(".slider-line").style.right = '0';
+});
 
 
 
@@ -81,18 +107,18 @@ symbol.forEach((elem)=>{
 });
 
 function setDeadline(object) {
-    let monthsArray = new Array ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+    // let monthsArray = new Array ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
 
-    let arr = object.dataset['date'].split(' ');
-    let day = arr[2];
-    let monthName = arr[1];
-    let year = arr[3];
+    let arr = object.dataset['date'].split('-').reverse();
+    let day = arr[0];
+    let month = arr[1];
+    let year = arr[2];
 
-    let month = monthsArray.findIndex((i)=> i === monthName) + 1;
+    // let month = monthsArray.findIndex((i)=> i === monthName) + 1;
 
-    if (month < 10) {
-        month = '0' + month;
-    }
+    // if (month < 10) {
+    //     month = '0' + month;
+    // }
 
     let date = `${day}.${month}.${year}`;
 
