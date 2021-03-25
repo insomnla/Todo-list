2fbdf5fd62abfb80cdb3c7c16568b4c870669325
insomnla <Epikmen@bk.ru>
2021-03-25 18:13:17 +0700

    add - jquery kekW

--- todolist/views/script/main.js
+++ todolist/views/script/main.js
@@ -138,6 +138,22 @@ categorisItem.forEach((elem)=>{
     });
 });
 
+$('.date-input').click(function(){
+    $(this).setCursorPosition(0);
+}).mask('99-99-9999', {placeholder:' '});
+
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+};
+
 
 function setDeadline(object) {
     // let monthsArray = new Array ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
